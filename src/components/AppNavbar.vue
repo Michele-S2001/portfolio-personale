226745faa0a612a1778e9dc1a23c3139daa6c59c
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-21 15:18:43 +0100

    ADD > Navabar styling

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -25,20 +25,44 @@
 <style lang="scss" scoped>
 @use '../styles/partials/vars' as *;
 .page-header {
-
+  position: fixed;
+  top: 20px;
+  left: 0;
+  right: 0;
   .navbar {
+    backdrop-filter: blur(10px);
     display: flex;
     gap: 20px;
-    padding: 20px 0;
+    padding: 20px 30px;
+    border-radius: 40px;
     font-size: 21px;
+    box-shadow: inset 0px 0px 8px 3px rgba($white, $alpha: 0.55);
 
     &__link {
+      position: relative;
+      transition: 0.2s ease-in;
       &:hover {
         color: $orange;
+
+        a::before {
+          width: 100%;
+          transform-origin: right;
+        }
       }
 
       a {
-        line-height: 50px;
+        line-height: 40px;
+          &::before {
+          content: '';
+          display: block;
+          position: absolute;
+          bottom: 0;
+          height: 2px;
+          width: 0;
+          background-color: $orange;
+          transform-origin: left;
+          transition: 0.2s ease-in;
+        }
       }
 
       &:first-child {
--- src/pages/PortfolioHome.vue
+++ src/pages/PortfolioHome.vue
@@ -1,112 +1,20 @@
 <template>
   <AppDefaulLayout>
-    <section class="home px-10">
-      <div class="container-sm">
-        <div class="home__inner">
-          <!-- left side -->
-          <div class="description">
-            <!-- saluti con nome -->
-            <div class="presentation">
-              <h1 class="presentation__main-title">Hi! I'm Michele Serafini</h1>
-              <p class="presentation__job">Full stack web-developer</p>
-            </div>
-            <!-- contatti -->
-            <div class="contacts">
-              <a class="icon" href="https://github.com/Michele-S2001" target="_blank">
-                <font-awesome-icon icon="fa-brands fa-github" size="2xl" />
-              </a>
-              <a class="icon" href="https://www.linkedin.com/in/michele-serafini-08574b1b7/" target="_blank">
-                <font-awesome-icon icon="fa-brands fa-linkedin" size="2xl" />
-              </a>
-              <a class="icon" href="mailto: serafini.michele01@gmail.com">
-                <font-awesome-icon icon="fa-regular fa-envelope" size="2xl"/>
-              </a>
-              <a class="icon" href="https://www.instagram.com/myser2001/">
-                <font-awesome-icon icon="fa-brands fa-instagram" size="2xl"/>
-              </a>
-            </div>
-          </div>
-
-          <!-- right side -->
-          <div class="thumbnail">
-            <img src="../../img/Picsart_24-01-25_16-12-02-368-PhotoRoom.png-PhotoRoom.png" alt="Portfolio image">
-          </div>
-        </div>
-      </div>
-    </section>
+    <AppHero/>
   </AppDefaulLayout>
 </template>
 
 <script>
 import AppDefaulLayout from '../layouts/AppDefaulLayout.vue';
+import AppHero from '../components/AppHero.vue';
   export default {
     components: {
-      AppDefaulLayout
+      AppDefaulLayout,
+      AppHero
     }
   }
 </script>
 
 <style lang="scss" scoped>
-@use '../styles/partials/vars' as *;
-.home {
-  padding-top: 100px;
-
-  &__inner {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 20px;
-
-    .description {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      padding: 50px 0;
-      user-select: none;
-
-      .presentation {
-        &__main-title {
-          font-size: 45px;
-          margin-bottom: 24px;
-        }
-
-        &__job {
-          font-size: 21px;
-          margin-bottom: 24px;
-        }
-      }
-
-      .contacts {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 14px;
-
-        .icon {
-          &:hover {
-            color: $orange;
-          }
-        }
-      }
-    }
-
-    .thumbnail img {
-      border-radius: 200px;
-    }
-  }
-}
-
-@media (max-width: 768px) {
-  .home {
-
-    &__inner {
-      display: block;
-
-      .thumbnail img {
-        margin: 0 auto;
-        max-height: 400px;
-      }
-    }
-  }
-}
-
 
 </style>
